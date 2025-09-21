90c6c8582ae66cfdb1e8ee67f9069ee1cc12f0a5
Ethan Pippin <ethanpippin2343@gmail.com>
2025-09-20 22:39:51 -0400

    5.10 support

---
+++ Package@swift-5.10.swift
@@ -0,0 +1,52 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import CompilerPluginSupport
+import PackageDescription
+
+let package = Package(
+    name: "StatefulMacros",
+    platforms: [
+        .iOS(.v16),
+        .tvOS(.v16),
+        .macOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "StatefulMacros",
+            targets: ["StatefulMacros"]
+        ),
+        .executable(
+            name: "StatefulMacrosClient",
+            targets: ["StatefulMacrosClient"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+        .package(url: "https://github.com/pointfreeco/swift-case-paths.git", from: "1.0.0"),
+    ],
+    targets: [
+        .macro(
+            name: "StatefulMacrosMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+                .product(name: "CasePaths", package: "swift-case-paths"),
+            ]
+        ),
+        .target(
+            name: "StatefulMacros",
+            dependencies: [
+                "StatefulMacrosMacros",
+                .product(name: "CasePaths", package: "swift-case-paths"),
+            ]
+        ),
+        .executableTarget(
+            name: "StatefulMacrosClient",
+            dependencies: [
+                "StatefulMacros",
+                .product(name: "CasePaths", package: "swift-case-paths"),
+            ]
+        ),
+    ]
+)
